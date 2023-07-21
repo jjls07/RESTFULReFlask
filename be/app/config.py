a519a630e5f3f08dde7f75040c47c4e62a0e57aa
jjls07 <jjkun07@gmail.com>
2023-07-20 23:44:46 -0500

    Update config.py

--- be/app/config.py
+++ be/app/config.py
@@ -5,13 +5,10 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
 
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:eurocorp@192.168.10.150:3306/td_utp'
-    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:eurocorp@localhost:3306/td_utp'
-    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:eurocorp@localhost:3306/carnetnuevo'
-    #SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123456789@localhost/carnet_utp"
 
-    # PRODUCCION
-    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://carnet:dTwM2023@localhost:3306/carnet"
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost:3306/reactrestful'
+
+
     
     # DEV
     SQLALCHEMY_DATABASE_URI = "sqlite:///datos.db"
